7da0a8c419cd625045c99b69f9564e977d00024f
Nathan <natefintak@gmail.com>
2025-03-07 17:02:13 -0500

    new tests in ShoppingCart

--- TDDOne/tddone/src/test/java/rocks/zipcode/ShoppingCartTest.java
+++ TDDOne/tddone/src/test/java/rocks/zipcode/ShoppingCartTest.java
@@ -1,6 +1,9 @@
 package rocks.zipcode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -12,43 +15,61 @@ class ShoppingCartTest {
     private ShoppingCart.Item item1;
     private ShoppingCart.Item item2;
     private ShoppingCart.Item item3;
+    private ShoppingCart.Item item4;
 
     @BeforeEach
     void setUp() {
-        item1 = new ShoppingCart.Item("Oranges", 1.75, 5);
-        item2 = new ShoppingCart.Item("Apples", 1.50, 5);
-        item3 = new ShoppingCart.Item("Cans", 2.00, 10);
+        //given this info
         shoppingCart = new ShoppingCart(0.09);
-
+        item1 = new ShoppingCart.Item("Orange", 1.75, 5);
+        item2 = new ShoppingCart.Item("Apple", 1.50, 5);
+        item3 = new ShoppingCart.Item("Can", 1.99, 4);
+        item4 = new ShoppingCart.Item("Peach", 1.75, 0);
     }
     // Make sure to test all functionality including:
     // - Adding and removing items
     @Test
-    void test1AddItem() {
+    public void test1AddItem() {
+        //when
+        shoppingCart.addItem(item1);
+        //then
+        assertEquals();
+    }
+
+    @Test
+    public void test2AddItem() {
+        //when
+        shoppingCart.addItem(item1);
+        shoppingCart.addItem(item2);
+        //then
+        assertEquals();
+    }
+
+    @Test
+    public void test3AddItem() {
+        //when
+        shoppingCart.addItem(item4);
+        shoppingCart.addItem(item3);
+        //then
+        assertEquals();
+        //when
+        shoppingCart.addItem(item1);
+        //then
+        assertEquals();
+    }
+
+    @Test
+    public void test1RemoveItem() {
 
     }
 
     @Test
-    void test2AddItem() {
+    public void test2RemoveItem() {
 
     }
 
     @Test
-    void test3AddItem() {
-
-    }
-
-    @Test
-    void test4AddItem() {
-
-    }
-
-    @Test
-    void test1RemoveItem() {
-
-    }
-    @Test
-    void test2RemoveItem() {
+    public void test3RemoveItem() {
 
     }
 
@@ -60,7 +81,10 @@ class ShoppingCartTest {
 
     // - Edge cases like an empty cart
     //check bankaccount tests for formatting, check shoppingcart for specific exception details
+    @Test
+    public void test1emptyCart() {
 
+    }
 
 
     // - Invalid inputs (e.g., negative prices, zero quantity)
