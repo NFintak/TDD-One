182c5b3eeb483fb95fe61af7ea3dd425f6b8bfc4
Nathan <natefintak@gmail.com>
2025-03-08 16:36:31 -0500

    addItem tests

--- TDDOne/tddone/src/test/java/rocks/zipcode/ShoppingCartTest.java
+++ TDDOne/tddone/src/test/java/rocks/zipcode/ShoppingCartTest.java
@@ -11,20 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
     // TODO: Write tests for the ShoppingCart class
-    private ShoppingCart shoppingCart;
-    private ShoppingCart.Item item1;
-    private ShoppingCart.Item item2;
-    private ShoppingCart.Item item3;
-    private ShoppingCart.Item item4;
+    ShoppingCart shoppingCart;
+    ShoppingCart fullCart;
+    ShoppingCart.Item item1;
+    ShoppingCart.Item item2;
+    ShoppingCart.Item item3;
+    ShoppingCart.Item item4;
 
     @BeforeEach
     void setUp() {
         //given this info
         shoppingCart = new ShoppingCart(0.09);
+        fullCart = new ShoppingCart(0.09);
         item1 = new ShoppingCart.Item("Orange", 1.75, 5);
         item2 = new ShoppingCart.Item("Apple", 1.50, 5);
         item3 = new ShoppingCart.Item("Can", 1.99, 4);
-        item4 = new ShoppingCart.Item("Peach", 1.75, 0);
+        item4 = new ShoppingCart.Item("Peach", 1.75, 1);
     }
     // Make sure to test all functionality including:
     // - Adding and removing items
@@ -33,7 +35,7 @@ class ShoppingCartTest {
         //when
         shoppingCart.addItem(item1);
         //then
-        assertEquals();
+        assertEquals(item1, shoppingCart.getItem(0));
     }
 
     @Test
@@ -42,29 +44,41 @@ class ShoppingCartTest {
         shoppingCart.addItem(item1);
         shoppingCart.addItem(item2);
         //then
-        assertEquals();
+        assertEquals(item2, shoppingCart.getItem(1));
     }
 
     @Test
     public void test3AddItem() {
         //when
-        shoppingCart.addItem(item4);
+        shoppingCart.addItem(item3);
         shoppingCart.addItem(item3);
         //then
-        assertEquals();
+        assertEquals(item3, shoppingCart.getItem(1));
         //when
         shoppingCart.addItem(item1);
         //then
-        assertEquals();
+        assertEquals(item1, shoppingCart.getItem(2));
     }
 
     @Test
     public void test1RemoveItem() {
-
+        //given
+        fullCart.addItem(item1);
+        fullCart.addItem(item2);
+        fullCart.addItem(item3);
+        //when
+        fullCart.removeItem(item2);
+        //then
+        assertEquals(item3, fullCart.getItem(1));
     }
 
     @Test
     public void test2RemoveItem() {
+        //given
+
+        //when
+
+        //then
 
     }
 
@@ -75,9 +89,20 @@ class ShoppingCartTest {
 
     // - Calculating subtotal, tax, and total
     //info for subtotal needed -
+    @Test
+    public void test1SubTotal() {
 
+    }
 
+    @Test
+    public void test2SubTotal() {
 
+    }
+
+    @Test
+    public void test3SubTotal() {
+
+    }
 
     // - Edge cases like an empty cart
     //check bankaccount tests for formatting, check shoppingcart for specific exception details
